7ec2509372aecad8d62d69b3e29ddcad03a22c29
SaboyaDev <saboyadev@gmail.com>
2020-10-22 23:58:34 -0400

    Created a footer and styled. Added a parallax effect with pure css. Styled the main navigation section. Changed the primary color

---
+++ js/index.js
@@ -0,0 +1,6 @@
+// const parallax = document.getElementById('parallax');
+
+// window.addEventListener('scroll', () => {
+// 	const offset = window.pageYOffset;
+// 	parallax.style.backgroundPositionY = offset * 0.7 + 'px';
+// });
